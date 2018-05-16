1d853dd686a82561b490f95c0c58bb77681ff626
DimitrivanManen <17382@ma-web.nl>
2018-05-16 16:09:43 +0200

    Update
    BlackHoles have been added and range buff on the planets

---
+++ SpaceGame/Assets/Script/BlackHoleMovement.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    }
+}
--- SpaceGame/Assets/Script/Gravity.cs
+++ SpaceGame/Assets/Script/Gravity.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class Gravity : MonoBehaviour {
 
+    //makes the variables and arrays it for multiple object use
     public GameObject[] planet;
+    public GameObject[] blackHole;
     public float gravity = 30f;
 
     private Rigidbody2D rig2d;
 
 	// Use this for initialization
 	void Start () {
+        //gets the objects
         rig2d = GetComponent<Rigidbody2D>();
         planet = GameObject.FindGameObjectsWithTag("Planet");
+        blackHole = GameObject.FindGameObjectsWithTag("BlackHole");
     }
 	
 	// Update is called once per frame
 	void Update () {
+        //this is used for the planet gravity pull
         for (int i = 0; i < planet.Length; i++)
         {
             Vector2 offset = planet[i].transform.position - transform.position;
@@ -26,6 +31,18 @@ public class Gravity : MonoBehaviour {
             if (gravitysqr > 0.001f && gravitysqr < 15)
             {
                 rig2d.AddForce(gravity * offset.normalized / gravitysqr);
+            }
+        }
+        //this is used for the blackhole pull and range
+        for (int i = 0; i < blackHole.Length; i++)
+        {
+            Vector2 offset = blackHole[i].transform.position - transform.position;
+
+            float gravitysqr = offset.sqrMagnitude;
+            float gravity2 = 5f;
+            if (gravitysqr > 0.001f && gravitysqr < 30)
+            {
+                rig2d.AddForce(gravity2 * offset.normalized / gravitysqr);
             }
         }
     }
