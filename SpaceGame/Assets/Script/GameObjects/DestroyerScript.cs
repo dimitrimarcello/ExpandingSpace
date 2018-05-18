30dc73c7f9cf2a8c3e6fce6b359e0483bdec328e
Elgianni <22642@ma-web.nl>
2018-05-18 11:37:02 +0200

    restore reset

---
+++ SpaceGame/Assets/Script/GameObjects/DestroyerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyerScript : MonoBehaviour {
+
+    //SceneController controller;
+    public GameObject[] obj;
+    private void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if(other.tag == "Player")
+        {
+            //Hier kan de gameover zijn
+            Debug.Break();
+            return;
+        }
+        else if (other.tag == "Destroyer")
+        {
+            Debug.Log(" ");
+            return;
+        }
+        else if(other.tag == "Planet")
+        {
+            Destroy(other.gameObject);
+            BuildMap();
+            return;
+
+        }
+        if (other.gameObject.transform.parent)
+        {
+            Destroy(other.gameObject.transform.parent.gameObject);
+        }
+        else
+        {
+            Destroy(other.gameObject);
+        }
+    }
+    public void BuildMap()
+    {
+        Vector2 spawnLocation = new Vector2(transform.position.x + Random.Range(23, 24), 4 - Random.Range(1, 8));
+        GameObject holes = Instantiate(obj[Random.Range(0, obj.GetLength(0))], spawnLocation, Quaternion.identity);
+    }
+
+}
---
+++ SpaceGame/Assets/Script/GameObjects/SpawnScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour {
+
+    public GameObject[] obj1;
+    public float spawnMin = 20;
+    public float spawnMax = 40;
+
+    // Use this for initialization
+    void Start () {
+        Spawn();
+	}
+	
+	void Spawn()
+    {
+        Vector2 spawnLocation = new Vector2(transform.position.x, transform.position.y - Random.Range(1, 8));
+        GameObject holes = Instantiate(obj1[Random.Range(0, obj1.GetLength(0))], spawnLocation, Quaternion.identity);
+        Invoke("Spawn", Random.Range(spawnMin,spawnMax));
+    }
+
+}
